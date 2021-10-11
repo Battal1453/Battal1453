6d844cdd22bf9e7076eef1a865f409ebc8323cf8
Battal1453 <92326568+Battal1453@users.noreply.github.com>
2021-10-11 12:04:53 -0700

    Update login.py
    versiyon geri alındı

--- login.py
+++ login.py
@@ -1,8 +1,11 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 while True:
     pass
 
 def kaydet():
     pass
+=======
+>>>>>>> parent of 393ac4e (Update login.py)
 =======
 >>>>>>> parent of 393ac4e (Update login.py)
